2bc94327a432760b2c6c2bb18f08b5ca7e0aebb8
Brianna Gordon <brigordon574@gmail.com>
2021-05-18 16:01:16 -0700

    commit tree d235b338dcbdafbca991701bc38aab75c06dcfce

--- index.js
+++ index.js
@@ -1,1 +1,77 @@
 // Code your solution in this file!
+
+const hq =42;
+
+function distanceFromHqInBlocks(blockNumber){
+    if (blockNumber < hq){
+        return  hq - blockNumber;
+    }
+    else {
+        if(blockNumber > hq){
+            return blockNumber - hq;
+        }
+        else {
+            if (blockNumber === hq){
+                return 0
+            }
+        }
+    }
+}
+    function distanceFromHqInFeet(blockNumber){
+        if (blockNumber < hq){
+        return (hq - blockNumber)*264 
+        }
+        else {
+            if (blockNumber > hq){
+                return (blockNumber - hq)*264
+            }
+        else {
+            if (blockNumber === hq){
+                return 0
+            }
+        }
+     }
+  }
+     const blockFeet = 264;
+
+    function distanceTravelledInFeet(start,finish){
+        if (start > finish ){
+          return (start - finish)*264;
+        }
+        else {
+          if (start < finish){
+              return(finish - start)*264;
+          } 
+          else {
+              if (start === finish){
+                  return 0
+              }
+          }
+        }
+    }
+
+      const flatRate = 25;
+
+    function calculatesFarePrice(start,finish){
+        const flatRate = 25;
+        if (distanceTravelledInFeet(start,finish) < 400){
+            return 0;
+        }
+        else {
+            if (distanceTravelledInFeet(start,finish) < 2000){ 
+            return (distanceTravelledInFeet(start,finish) -400) * 0.02;
+        }  
+       else { 
+           if ( 2000 < distanceTravelledInFeet(start,finish) && distanceTravelledInFeet(start,finish) < 2500){
+               return (flatRate);
+           }
+           else {
+               if (distanceTravelledInFeet(start,finish) >= 2500){
+                   return ("cannot travel that far"); 
+               }
+           }
+        }
+
+    }  
+        
+ }
